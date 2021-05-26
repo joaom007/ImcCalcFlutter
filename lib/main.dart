5216d3dcccecb08f850b161261fc0a47bbb92c2e
João Marcos <joaomarcossm2013@hotmail.com>
2021-05-26 18:32:49 -0300

    aula 19-05 lógica de calculo e reset campos

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MaterialApp(
@@ -12,6 +13,41 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController pesoController = TextEditingController();
+  TextEditingController alturaController = TextEditingController();
+
+  String _textoInfo = "Informe seus dados";
+
+  void _limparCampos() {
+    pesoController.text = "";
+    alturaController.text = "";
+    setState(() {
+      _textoInfo = "Informe seus dados";
+    });
+  }
+
+  void _calcularImc() {
+    setState(() {
+      double peso = double.parse(pesoController.text);
+      double altura = double.parse(alturaController.text);
+      double imc = peso / pow(altura, 2);
+
+      if (imc < 18.6) {
+        _textoInfo = "Abaixo do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc <= 24.9) {
+        _textoInfo = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      } else if (imc <= 29.9) {
+        _textoInfo = "Levemente acima do peso (${imc.toStringAsPrecision(3)})";
+      } else if (imc <= 34.9) {
+        _textoInfo = "Obesidade grau I (${imc.toStringAsPrecision(3)})";
+      } else if (imc <= 39.9) {
+        _textoInfo = "Obesidade grau II (${imc.toStringAsPrecision(3)})";
+      } else {
+        _textoInfo = "Obesidade grau III (${imc.toStringAsPrecision(3)})";
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,7 +58,7 @@ class _HomeState extends State<Home> {
         actions: <Widget>[
           IconButton(
             icon: Icon(Icons.refresh),
-            onPressed: () {},
+            onPressed: _limparCampos,
           )
         ],
       ),
@@ -44,6 +80,7 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 25.0),
+              controller: pesoController,
             ),
             TextField(
               keyboardType: TextInputType.number,
@@ -53,13 +90,15 @@ class _HomeState extends State<Home> {
               ),
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 25.0),
+              controller: alturaController,
             ),
             Container(
               padding: EdgeInsets.only(top: 10, bottom: 10),
               height: 50.0,
+              // ignore: deprecated_member_use
               child: RaisedButton(
                 //Decorated button nova versão
-                onPressed: () {},
+                onPressed: _calcularImc,
                 child: Text(
                   "Calcular",
                   style: TextStyle(fontSize: 25.0, color: Colors.white),
@@ -68,7 +107,7 @@ class _HomeState extends State<Home> {
               ),
             ),
             Text(
-              "Info",
+              _textoInfo,
               textAlign: TextAlign.center,
               style: TextStyle(color: Colors.green, fontSize: 25.0),
             ),
